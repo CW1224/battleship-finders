ca57f2241c630f3a53c57680f42d58f9d476aff1
CW1224 <xixiwang1224@gmail.com>
2022-09-22 14:18:54 +0000

    Added in the instructions for the game

--- run.py
+++ run.py
@@ -11,8 +11,33 @@ def accept_game():
         show_instructions()
 
 
+def show_instructions():
+    '''
+    Function that gives the user a choice to read or not read the instructions.
+    Choosing yes would allow the user to read the instructions. 
+    Choosing no would bring the user to the next part of the game.
+    '''
+    print("The goal of this game is to sink all of the opponant's ships.")
+    print("While making sure that they don't sink yours.")
+    print("You will have a choice between grid sizes of 6x6 or 10x10.")
+    print("Both players will have a total of four ships.")
+    print("You will have a total of 24 bullots.")
+    print("Choose where you want to aim by typing in th co-ordinates.")
+    print("The co-ordinates should be expressed as x,y.")
+    print("An example would be 3,4 .")
+    print("The top left of the grid is 1,1 .")
+    print("As you go across the grid, the first number(x) increases.")
+    print("As you go down the grid, the second number (y) increases.")
+    print("The game ends when all the ships have been sank or when the bullets run out.")
+    print("Think carefully and enjoy the game!\n")
+
+    instructions_read = input("Press 1 when you have finished reading the instructions.")
+
+
 print("Welcome to Battleship Attack!\n")
 
 
 def main():
     accept_game()
+
+main()
