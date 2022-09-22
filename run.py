1243ff0a276ef1a585afc387dca77e07e9857a4b
CW1224 <xixiwang1224@gmail.com>
2022-09-22 20:18:05 +0000

    Added in the difficulty level choices

--- run.py
+++ run.py
@@ -5,15 +5,19 @@ def accept_game():
     Choosing no means that the user would stay on the same page.
     '''
     print("Would you like to start the game?")
-    user_accept = int(input("Press 1 for yes, press 0 for no\n"))
+    user_accept = int(input("Press 1 for yes\n"))
     print("")
     
+    # Pressing 1 would allow the instruction question to show.
     if user_accept == 1:
         print("Would you like to read the instructions?")
         instructions = int(input("Press 1 for yes, 0 for no\n"))
         print("")
         if instructions == 1:
             show_instructions()
+        # Pressing 0 would allow the user to skip the instructions and go straight to the page where the difficulty level is picked.
+        else:
+            difficulty_level()
 
 
 def show_instructions():
@@ -35,14 +39,17 @@ def show_instructions():
     print("The game ends when all the ships have been sank or when your bullet runs out.")
     print("Think carefully and enjoy the game!\n")
 
-    instructions_read = input("Press 1 when you have finished reading the instructions.")
+    instructions_read = int(input("Press 0 when you have finished reading the instructions.\n"))
+    print("")
+
+    if instructions_read == 0:
+        difficulty_level()
+
+def difficulty_level():
+    print("Which type of challenge are you up for?")
+    user_level_choice = int(input("Press 1 for easy and 2 for hard\n"))
 
 
 print("Welcome to Battleship Attack!\n")
 
-
-def main():
-    accept_game()
-
-
-main()
+accept_game()
