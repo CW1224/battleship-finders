e9480c2eb2b537f28875241fbe8fbd380d9e1a8a
CW1224 <xixiwang1224@gmail.com>
2022-09-22 14:00:13 +0000

    Added in the accept challenge function

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def accept_game():
+    '''
+    Function for the user to accept the challenge. 
+    Choosing yes would bring the user to the next part of the game. 
+    Choosing no means that the user would stay on the same page.
+    '''
+    print("Would you like to start the game?")
+    user_accept = input("Press 1 for yes, press 0 for no\n")
+    
+    if user_accept == 1:
+        show_instructions()
+
+
+print("Welcome to Battleship Attack!\n")
+
+
+def main():
+    accept_game()
